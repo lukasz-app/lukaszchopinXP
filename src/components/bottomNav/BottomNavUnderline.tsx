69fffc8ddeb74ca93bfc043d782de67941ea0f68
lukasz <lukasz.chludzinski@gmail.com>
2019-01-19 04:47:12 +0100

    android dev-trigger fix

--- src/components/bottomNav/BottomNavUnderline.tsx
+++ src/components/bottomNav/BottomNavUnderline.tsx
@@ -1,6 +1,14 @@
 import React from 'react';
-import { Component, View, Styles } from 'reactxp';
+import {
+  Component, View, Styles, Animated,
+} from 'reactxp';
 import { observer } from 'mobx-react';
+import {
+  StyleRuleSet,
+  StyleRuleSetOrArray,
+  AnimatedViewStyle,
+  AnimatedValue,
+} from 'reactxp/dist/common/Types';
 import styles from './styles';
 
 interface Props {
@@ -9,20 +17,35 @@ interface Props {
     itemCount: number;
 }
 
+interface State {
+    index: number;
+    totalWidth: number;
+    itemCount: number;
+    animValue: AnimatedValue;
+}
+
 @observer
-export default class Underline extends Component<Props, {}> {
-  render() {
-    const { totalWidth, itemCount, index } = this.props;
-    const lineWidth = totalWidth / itemCount;
-    const left = index * lineWidth + 10;
-    console.log(left);
-    return (
-      <View
-        style={[
-          styles.underlineContainer,
-          Styles.createViewStyle({ left, width: lineWidth - 20 }, false),
-        ]}
-      />
-    );
-  }
+export default class Underline extends Component<Props, State> {
+    state = {
+      animValue: Animated.createValue(0),
+    };
+
+    componentDidUpdate(prevProps: Props) {
+      const { index } = this.props;
+      const {
+        // totalWidth: totalWidthPrev,
+        // itemCount: itemCountPrev,
+        index: indexPrev,
+      } = prevProps;
+      console.log('CDU');
+      console.log('CDU old index DIFFER prev index', index !== indexPrev);
+    }
+
+    render() {
+      const { totalWidth, itemCount, index } = this.props;
+      const lineWidth = totalWidth / itemCount;
+      const left = index * lineWidth + 10;
+      console.log(left);
+      return <View style={styles.underlineContainer} />;
+    }
 }
